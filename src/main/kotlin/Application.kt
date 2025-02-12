9eb1abe0f5d62840aaed4500f1380d6024b0da07
Mansoor Sajjad <mansoor.sajjad@entur.org>
2025-02-12 11:54:05 +0100

    Routing the Hard way

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,16 +1,27 @@
 package com.mansoor.ktor
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 // Extension function on class Application.
+// To create the Application i Ktor we add this extension function to the Application class.
 fun Application.module() {
 
-    val port = environment.config.propertyOrNull("ktor.deployment.port")?.getString() ?: "9999"
-
     configureMonitoring()
-    configureRouting()
+//    configureRouting()
+    // Lets do routing the hard way
+
+    install(RoutingRoot) {
+        route("/", HttpMethod.Get) {
+            handle {
+                call.respondText("Hello from routing")
+            }
+        }
+    }
 }
