92bcbcb45244c0f913dac33fc336a3c525516aa3
Mansoor Sajjad <mansoor.sajjad@entur.org>
2025-02-12 11:24:04 +0100

    Read the configuration properties inside the code.

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -6,7 +6,11 @@ fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+// Extension function on class Application.
 fun Application.module() {
+
+    val port = environment.config.propertyOrNull("ktor.deployment.port")?.getString() ?: "9999"
+
     configureMonitoring()
     configureRouting()
 }
