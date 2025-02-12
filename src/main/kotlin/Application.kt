5063d177ab385dc971263d32cf9638a9601c8ae7
Mansoor Sajjad <mansoor.sajjad@entur.org>
2025-02-12 10:58:13 +0100

    Start ktor project with Intellij

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package com.mansoor.ktor
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureRouting()
+}
